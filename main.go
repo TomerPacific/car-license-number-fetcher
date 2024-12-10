35619f30ca8fbabab7d492aa3fb98b934649b043
unknown <tomerpacific@gmail.com>
2024-12-10 20:50:33 +0200

    car-license-number-fetcher adding more fields to struct

--- main.go
+++ main.go
@@ -5,23 +5,28 @@ import (
 )
 
 type Vehicle struct {
-	ID              int       `json:"id"`
-	Vehicle_Number  int       `json:"mispar_rechev"`
-	Origin_Id       int       `json:"tozeret_cd"`
-	Model_Type      int       `json:"sug_degem"`
-	Origin_Country  string    `json:"tozeret_nm"`
-	Model_Id        int       `json:"degem_cd"`
-	Model_Number    int       `json:"degem_nm"`
-	Trim_Level      string    `json:"ramat_gimur"`
-	Safety_Level    int       `json:"ramat_eivzur_betihuty"`
-	Pollution_Level int       `json:"kvutzat_zihum"`
-	Production_Year int       `json:"shnat_yitzur"`
-	Engine_Model    string    `json:"degem_manoa"`
-	Last_Test_Date  time.Time `json:"mivchan_acharon_dt"`
-	Expiration      time.Time `json:"tokef_dt"`
-	Ownership       string    `json:"baalut"`
-	Frame           string    `json:"misgeret"`
-	Color_Id        string    `json:"tzeva_cd"`
-	Color_Name      string    `json:"tzeva_rechev"`
-	Front_Tire_Code string    `json:"zmig_kidmi"`
+	ID               int       `json:"id"`
+	Vehicle_Number   int       `json:"mispar_rechev"`
+	Origin_Id        int       `json:"tozeret_cd"`
+	Model_Type       int       `json:"sug_degem"`
+	Origin_Country   string    `json:"tozeret_nm"`
+	Model_Id         int       `json:"degem_cd"`
+	Model_Number     int       `json:"degem_nm"`
+	Trim_Level       string    `json:"ramat_gimur"`
+	Safety_Level     int       `json:"ramat_eivzur_betihuty"`
+	Pollution_Level  int       `json:"kvutzat_zihum"`
+	Production_Year  int       `json:"shnat_yitzur"`
+	Engine_Model     string    `json:"degem_manoa"`
+	Last_Test_Date   time.Time `json:"mivchan_acharon_dt"`
+	Expiration       time.Time `json:"tokef_dt"`
+	Ownership        string    `json:"baalut"`
+	Frame            string    `json:"misgeret"`
+	Color_Id         string    `json:"tzeva_cd"`
+	Color_Name       string    `json:"tzeva_rechev"`
+	Front_Tire_Code  string    `json:"zmig_kidmi"`
+	Rear_Tire_Code   string    `json:"zmig_ahori"`
+	Fuel_Type        string    `json:"sug_delek_nm"`
+	Registry_Command int       `json:"horaat_rishum"`
+	First_On_Road    string    `json:"moed_aliya_lakvish"`
+	Commerical_Name  string    `json:"kinuy_mishari"`
 }
