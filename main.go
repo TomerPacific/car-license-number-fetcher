dfc8ffb7b28b83edae023aca3ad44921cea30dff
unknown <tomerpacific@gmail.com>
2024-12-13 12:11:18 +0200

    car-license-number-fetcher renaming all errors

--- main.go
+++ main.go
@@ -22,8 +22,8 @@ func main() {
 	if port == "" {
 		port = "8080"
 	}
-	if err := router.Run(":" + port); err != nil {
-		log.Panicf("error: %s", err)
+	if runningServerError := router.Run(":" + port); runningServerError != nil {
+		log.Panicf("Running server encountered an error: %s", runningServerError)
 	}
 }
 
@@ -31,23 +31,23 @@ func getVehiclePlateNumber(c *gin.Context) {
 	licensePlate := c.Param("licensePlate")
 	requestUrl := fmt.Sprintf("%s%s", endpoint, licensePlate)
 
-	res, err := http.Get(requestUrl)
-	if err != nil {
-		fmt.Printf("Error fetching license plate: %s\n", err)
+	res, requestError := http.Get(requestUrl)
+	if requestError != nil {
+		fmt.Printf("Error fetching license plate: %s\n", requestError)
 		os.Exit(1)
 	}
 
-	resBody, err := io.ReadAll(res.Body)
-	if err != nil {
-		fmt.Printf("error parsing response: %s\n", err)
+	resBody, readingResponseError := io.ReadAll(res.Body)
+	if readingResponseError != nil {
+		fmt.Printf("Error parsing response: %s\n", readingResponseError)
 		os.Exit(1)
 	}
 
 	var v vehicle.Vehicle
-	er := json.Unmarshal(resBody, &v)
+	convertingToJsonError := json.Unmarshal(resBody, &v)
 
-	if er != nil {
-		fmt.Printf("error converting response: %s\n", er)
+	if convertingToJsonError != nil {
+		fmt.Printf("Error converting response: %s\n", convertingToJsonError)
 		os.Exit(1)
 	}
 
