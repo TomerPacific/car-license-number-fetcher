bc0dba8de280caedeeaea109ea7c87d7c7947865
unknown <tomerpacific@gmail.com>
2025-01-17 16:11:09 +0200

    car-license-number-fetcher returning colon to port

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func main() {
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080"
+		port = ":8080"
 	}
 
 	if runningServerError := router.RunTLS(port, certFile, keyFile); runningServerError != nil {
