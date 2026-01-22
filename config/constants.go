c41fbea9bdeb59c0d535a018fe29a633de5d95aa
Tomer Ben-Rachel <tomerpacific@gmail.com>
2026-01-22 22:11:24 +0200

    CarLicenseNumberFetcher renaming constant

--- config/constants.go
+++ config/constants.go
@@ -1,12 +1,13 @@
 package config
 
 const (
-	Endpoint           = "https://data.gov.il/api/3/action/datastore_search?resource_id=053cea08-09bc-40ec-8f7a-156f0677aff3&limit=1&q="
-	LicensePlateKey    = "licensePlate"
-	VehicleNameKey     = "vehicleName"
-	DefaultPort        = "8080"
-	OpenAIAPIKeyEnvVar = "OPENAPI_KEY"
+	VehicleDataAPIEndpoint = "https://data.gov.il/api/3/action/datastore_search?resource_id=053cea08-09bc-40ec-8f7a-156f0677aff3&limit=1&q="
+	WheelSizeAPIEndpoint   = "https://api.wheel-size.com/v2/search/by_model/"
+	LicensePlateKey       = "licensePlate"
+	VehicleNameKey        = "vehicleName"
+	DefaultPort           = "8080"
+	OpenAIAPIKeyEnvVar    = "OPENAPI_KEY"
 	WheelSizeAPIKeyEnvVar = "WHEEL_SIZE_KEY"
-	MobileUserAgent    = "Ktor client"
-	ErrorKey           = "error"
+	MobileUserAgent       = "Ktor client"
+	ErrorKey              = "error"
 )
