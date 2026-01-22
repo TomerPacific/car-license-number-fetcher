a27df7ab755aa9da16c33b81ff2cd1c8e5d383c0
Tomer Ben-Rachel <tomerpacific@gmail.com>
2026-01-22 22:12:25 +0200

    CarLicenseNumberFetcher removing unnecessary struct

--- models/tirePressureResponse.go
+++ models/tirePressureResponse.go
@@ -1,23 +1,5 @@
 package tirePressure
 
-import (
-	"context"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"net"
-	"net/http"
-	"time"
-)
-
-type TirePressurePayload struct {
-	Make         string `json:"make"`
-	Model        string `json:"model"`
-	Year         int    `json:"year"`
-	Modification string `json:"modification,omitempty"`
-	TireSize     string `json:"tireSize,omitempty"`
-}
-
 type TirePressureResponse struct {
 	Source   string      `json:"source"`
 	FrontPsi *float64    `json:"frontPsi,omitempty"`
