45c00bbe69bbea5341b1dc0bfab7b8b1fc05213c
Tomer Ben-Rachel <tomerpacific@gmail.com>
2026-01-23 06:55:49 +0200

    CarLicenseNumberFetcher adding constant for region

--- config/constants.go
+++ config/constants.go
@@ -3,6 +3,7 @@ package config
 const (
 	VehicleDataAPIEndpoint = "https://data.gov.il/api/3/action/datastore_search?resource_id=053cea08-09bc-40ec-8f7a-156f0677aff3&limit=1&q="
 	WheelSizeAPIEndpoint   = "https://api.wheel-size.com/v2/search/by_model/"
+	WheelSizeDefaultRegion = "eudm"
 	LicensePlateKey       = "licensePlate"
 	VehicleNameKey        = "vehicleName"
 	DefaultPort           = "8080"
