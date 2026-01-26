2adabe4008294ac4f5dd395ddbb0ec25b712f914
Tomer Ben-Rachel <tomerpacific@gmail.com>
2026-01-26 22:16:29 +0200

    CarLicenseNumberFetcher using serrors

--- utils/http_utils.go
+++ utils/http_utils.go
@@ -2,31 +2,29 @@ package utils
 
 import (
 	"net/http"
-	"strings"
+	serrors"car-license-number-fetcher/serrors"
 
 	config "car-license-number-fetcher/config"
 
 	"github.com/gin-gonic/gin"
+	"errors"
 )
 
-// RespondWithError sends an error response with the specified status code
 func RespondWithError(c *gin.Context, code int, err error) {
 	c.JSON(code, gin.H{config.ErrorKey: err.Error()})
 }
 
-// HandleVehicleDetailsError maps vehicle details fetch errors to appropriate HTTP responses
 func HandleVehicleDetailsError(c *gin.Context, err error, licensePlate string) {
-	errMsg := err.Error()
-
-	if strings.Contains(errMsg, "error fetching license plate") {
-		RespondWithError(c, http.StatusBadGateway, err)
-	} else if strings.Contains(errMsg, "error parsing response") || strings.Contains(errMsg, "error converting response") {
-		RespondWithError(c, http.StatusInternalServerError, err)
-	} else if strings.Contains(errMsg, "response was not successful") || strings.Contains(errMsg, "no matching vehicle") {
-		RespondWithError(c, http.StatusNotFound, err)
-	} else if strings.Contains(errMsg, "error converting safetyFeaturesLevel") {
-		RespondWithError(c, http.StatusNotFound, err)
-	} else {
-		RespondWithError(c, http.StatusInternalServerError, err)
-	}
+    switch {
+    case errors.Is(err, serrors.ErrFetchLicensePlate):
+        RespondWithError(c, http.StatusBadGateway, err)
+    case errors.Is(err, serrors.ErrParseResponse),
+         errors.Is(err, serrors.ErrConvertSafetyFeaturesLevel):
+        RespondWithError(c, http.StatusInternalServerError, err)
+    case errors.Is(err, serrors.ErrResponseNotSuccessful),
+         errors.Is(err, serrors.ErrNoMatchingVehicle):
+        RespondWithError(c, http.StatusNotFound, err)
+    default:
+        RespondWithError(c, http.StatusInternalServerError, err)
+    }
 }
